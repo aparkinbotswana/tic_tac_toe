d1cc6b37a59ef8e009636d8074b58247177fff3c
James Theo <jamestheo@Jamess-MacBook-Pro.local>
2017-05-30 15:37:22 +1000

    began work on win conditions. 'X' and 'O' values pushed into array where evaluation of win condition is to be made.

--- JS/tictactoe.js
+++ JS/tictactoe.js
@@ -1,24 +1,41 @@
 
+
+
 $(document).ready(function() {
   console.log("linked");
 
   var player = true
+  var x = "X"
+  var o = "O"
+
+  var outcome = ["_", "_", "_", "_", "_", "_", "_", "_", "_"];
 
   var playGame = function(e) {
-
+    // console.log(outcome);
+    var i;
 
     if (player === true) {
       // console.log("player 1 turn " + player);
-      $(this).html("X")
+      $(this).html(x)
+      i = parseInt(this.id);
+      outcome[i] = 'X';
+
+      console.log(outcome);
 
     } else {
       // console.log("player 2 turn " + player);
-      $(this).html("O")
+      $(this).html(o)
+      i = parseInt(this.id);
+      outcome[i] = 'O';
+
+      console.log(outcome);
 
     }
 
     // cycles through the player 1 and player 2.
     if (player){player=false}else player = true;
+
+
   }
 
 
@@ -26,6 +43,7 @@ $(document).ready(function() {
 
 
 });
+
 
 
 //
@@ -91,6 +109,4 @@ $(document).ready(function() {
 // Break the project down into different components (data, presentation, views, style, DOM manipulation) and brainstorm each component individually. Use whiteboards!
 // Use your Development Tools (console.log, inspector, alert statements, etc) to debug and solve problems
 // Work through the lessons in class, ask questions and come to office hours when you need to. Think about adding relevant code to your Tic Tac Toe game each night, instead of, you know... procrastinating.
-// Commit early, commit often. Don’t be afraid to break something because you can always go back in time to a previous version.
 // Check out Tutorial and Documentation resources (jQuery tutorial) at home to better understand what you’ll be getting into.
-// Don’t be afraid to write code that you know you will have to remove later. Create temporary elements (buttons, links, etc) that trigger events if real data is not available. For example, if you’re trying to figure out how to change some text when the game is over but you haven’t solved the win/lose game logic, you can create a button to simulate that until then.
