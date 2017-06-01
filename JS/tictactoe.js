f157b79188f62a14c57dd7ae6fcfef6301cc986a
James Theo <jamestheo@Jamess-MacBook-Pro.local>
2017-06-01 22:06:28 +1000

    have come across bug in win condition where draw statement is returned on final move even if there is a win. attempting to pinpoint error.

--- JS/tictactoe.js
+++ JS/tictactoe.js
@@ -3,11 +3,12 @@
 
 $(document).ready(function() {
 
-  // var player = true
 
-  var outcome = ["_", "_", "_", "_", "_", "_", "_", "_", "_"];
+  var outcome = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]; //
+
   var turns = 0;
-  var gameOver = false;
+
+  var gameOver = false; // gameOver variable
 
   var winCondition = [
     [0, 1, 2],
@@ -20,32 +21,26 @@ $(document).ready(function() {
     [2, 4, 6]];
 
 
-  var gameWon = function(){
+  var gameWon = function(){ // function called after every human and computer move to determine if there has been a win. turns variable is increased by one in every click function to determine when all moves have been made. if there is no win condition met when all boxes have been taken, it will declare a draw
 
     for (var i = 0; i < winCondition.length; i++) {
       condition = winCondition[i]
-        // console.log(winCondition[i]);
         var win = []
       for (var j = 0; j < condition.length; j++) {
         var position = condition[j];
         win.push(outcome[position]);
         var final = win.join('')
-        console.log(final);
-
+        console.log(condition);
         if (final === "XXX") {
           $("#gameMessageX").fadeIn()
-
-          console.log("Winnder!");
           return true;
+
         } else if (final === "OOO") {
           $("#gameMessageO").fadeIn()
-
-          console.log("O is WINDER");
           return true;
+
         } else if (turns === 5) {
           $("#gameMessageDraw").fadeIn()
-
-          console.log("draw");
           return false;
         }
       }
@@ -57,22 +52,18 @@ $(document).ready(function() {
 
 
   var playGame = function() {
-    // encompassing if statement checking if a player has already taken a move in a box and prevents other player from changing it
+    //  if statement checking if a player has already taken a move in a box and prevents them or the computer  from changing it
     if ($(this).html() === "X" || $(this).html() === "O") {
       return;
     }
 
     var i;
     turns +=1;
-    // if (player) {player = false} else player = true;
 
-    // if (player === true) {
-
-    // interaction with DOM to visually represent "X" on the gameboard.
     $(this).html("X")
-    i = parseInt(this.id);
-    // pushes the players move into an array which assesses victory conditions
-    outcome[i] = "X";
+    i = parseInt(this.id); // interaction with DOM to visually represent "X" on the gameboard.
+
+    outcome[i] = "X"; // pushes the players move into an array which assesses victory conditions
 
     gameOver = gameWon();
     if( gameOver ) {
@@ -96,16 +87,20 @@ $(document).ready(function() {
         $('#' + "4").html("O");
         break;
 
-      } else if (true) {
+      } else if (outcome[2] === '_') {
+        outcome[2] = 'O';
+        $('#' + "2").html("O");
+        break;
 
-      } else if (outcome[i] === '_') {
+      }
+
+      else if (outcome[i] === '_') {
         outcome[i] = 'O';
         $('#' + i.toString()).html("O");
         break;
       }
     }
-
-    gameOver = gameWon();
+    gameOver = gameWon(); //computer won!
 
 
 
@@ -118,66 +113,3 @@ $(document).ready(function() {
 
 
 });
-
-
-
-//
-// Big Goals
-//
-// DONE----------------------------
-// Build a dynamic game that allows two players to compete
-
-// Craft a readme.md file that explains your app to the world
-// Technical Requirements
-//
-// Your app must:
-//
-
-// DONE----------------------------
-// Render a game board in the browser
-
-// DONE----------------------------
-// Switch turns between X and O (or whichever markers you select)
-
-
-
-// Visually display which side won if a player gets three in a row or show a draw/"cat’s game" if neither wins ---------------------Perhaps include a hidden div which only shows when the game has finished with relevant message
-
-// DONE--------------------------------
-// Include separate HTML / CSS / JavaScript files
-
-// DONE-------------------------------
-// Use Javascript for DOM manipulation
-
-// DONE-------------------------------
-// Deploy your game online, where the rest of the world can access it
-
-
-
-
-
-
-
-
-// Bonus
-// These are for extra credit! Don't focus on these until you've hit the core requirements.
-//
-// Keep track of multiple game rounds with a win counter
-// Allow players to customize their tokens (X, O, name, picture, etc)
-// Get inventive with your styling, e.g. use hover effects or animations to spiff things up
-// Use LocalStorage to persist data locally to allow games to continue after page refresh or loss of internet connectivity
-// Support custom board sizes: default is 3x3 but you could allow users to choose a larger board
-// Support networked multiplayer: https://www.firebase.com/ has a nice quickstart guide
-// TRICKIEST: Create an AI opponent: teach Javascript to play an unbeatable game against you
-// Necessary Deliverables
-//
-// A working game, built by you, hosted somewhere on the internet
-// A link to your hosted working game in the URL section of your Github repo
-// A git repository hosted on Github, with a link to your hosted game, and frequent commits dating back to the very beginning of the project
-// A readme.md file with explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
-// Suggested Ways to Get Started
-//
-// Break the project down into different components (data, presentation, views, style, DOM manipulation) and brainstorm each component individually. Use whiteboards!
-// Use your Development Tools (console.log, inspector, alert statements, etc) to debug and solve problems
-// Work through the lessons in class, ask questions and come to office hours when you need to. Think about adding relevant code to your Tic Tac Toe game each night, instead of, you know... procrastinating.
-// Check out Tutorial and Documentation resources (jQuery tutorial) at home to better understand what you’ll be getting into.
